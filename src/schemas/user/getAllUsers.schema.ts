d3db74977745a14c9d32b04df9fce1aad73d5244
Matheus M <matheus150101miranda@gmail.com>
2023-06-20 14:55:24 -0300

    fix errors and refactoring getAllUsersService

--- src/schemas/user/getAllUsers.schema.ts
+++ src/schemas/user/getAllUsers.schema.ts
@@ -1,11 +1,45 @@
 import * as yup from "yup";
-import { IUserResponse } from "../../interfaces/user";
+import {
+  IUserResponse,
+  IUserResponseIsNotRecruiter,
+} from "../../interfaces/user";
 
-export const getAllUsersSchema: yup.SchemaOf<IUserResponse[]> = yup.array(
-  yup.object().shape({
-    isRecruiter: yup.boolean().required(),
-    email: yup.string().required(),
-    name: yup.string().required(),
-    id: yup.string().required(),
-  })
-);
+export const getAllUsersSchema: yup.SchemaOf<IUserResponseIsNotRecruiter[]> =
+  yup.array(
+    yup.object().shape({
+      tech: yup.object().shape({
+        html: yup.boolean().required(),
+        css: yup.boolean().required(),
+        js: yup.boolean().required(),
+        react: yup.boolean().required(),
+        ts: yup.boolean().required(),
+        angular: yup.boolean().required(),
+        vuejs: yup.boolean().required(),
+        php: yup.boolean().required(),
+        c: yup.boolean().required(),
+        node: yup.boolean().required(),
+        sass: yup.boolean().required(),
+      }),
+      city: yup.string().required(),
+      schooling: yup.string().required(),
+      vacancy: yup.string().required(),
+      isWork: yup.boolean().required(),
+      linkedin: yup.string().required(),
+      github: yup.string().required(),
+      portfolio: yup.string().notRequired().nullable(),
+      isRecruiter: yup.boolean().required(),
+      email: yup.string().email().required(),
+      name: yup.string().required(),
+      id: yup.string().required(),
+    })
+  );
+
+export const getAllUsersSchemaIsRecruiter: yup.SchemaOf<IUserResponse[]> =
+  yup.array(
+    yup.object().shape({
+      isRecruiter: yup.boolean().required(),
+      email: yup.string().email().required(),
+      name: yup.string().required(),
+      id: yup.string().required(),
+    })
+  );
--- src/services/user/getAllUsers.service.ts
+++ src/services/user/getAllUsers.service.ts
@@ -1,13 +1,38 @@
 import { IUserResponse } from "../../interfaces/user";
-import { getAllUsersSchema } from "../../schemas/user/getAllUsers.schema";
+import {
+  getAllUsersSchema,
+  getAllUsersSchemaIsRecruiter,
+} from "../../schemas/user/getAllUsers.schema";
 import { userRepo } from "../../utils/repositories";
 
 export const getAllUsersService = async (): Promise<IUserResponse[]> => {
-  const users = await userRepo.find();
-
-  const usersValidated = await getAllUsersSchema.validate(users, {
-    stripUnknown: true,
+  const usersIsNotRecruiter = await userRepo.find({
+    where: { isRecruiter: false },
+    relations: { tech: true },
   });
 
-  return usersValidated;
+  const usersValidatedIsNotRecruiter = await getAllUsersSchema.validate(
+    usersIsNotRecruiter,
+    {
+      stripUnknown: true,
+    }
+  );
+
+  const usersIsRecruiter = await userRepo.find({
+    where: { isRecruiter: true },
+  });
+
+  const usersValidatedIsRecruiter = await getAllUsersSchemaIsRecruiter.validate(
+    usersIsRecruiter,
+    {
+      stripUnknown: true,
+    }
+  );
+
+  const usersJoin = [
+    ...usersValidatedIsNotRecruiter,
+    ...usersValidatedIsRecruiter,
+  ];
+
+  return usersJoin;
 };
